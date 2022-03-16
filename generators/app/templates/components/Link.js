3e793313740859db9e2222d0ae871be013fdf875
Stephan Ringel <ringel@adobe.com>
2022-03-16 16:51:46 +0100

    Fix issues in the template

--- generators/app/templates/components/Link.js
+++ generators/app/templates/components/Link.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 // Until https://jira.corp.adobe.com/browse/ACNA-1497 is fixed
 export default ({children, href = '', ...props}) => {
   return (
-    <Link href={`/api/v1/web/actions/pages${href}`} {...props}>
+    <Link href={process.env.NODE_ENV === 'production' ? `/api/v1/web/actions/pages${href}` : href} {...props}>
       {children}
     </Link>
   )
