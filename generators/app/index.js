103f3dbcf56206dca81d143cfc5d7d8bd26b617e
Stephan Ringel <ringel@adobe.com>
2022-03-16 16:38:45 +0100

    Fix issues in the template

--- generators/app/index.js
+++ generators/app/index.js
@@ -8,33 +8,35 @@ module.exports = class extends Generator {
         this.answers = await this.prompt([
             {
                 type: 'input',
-                name: 'NAME',
-                message: 'Set Store name'
+                name: 'APP_NAME',
+                message: 'Sites app name',
+                default: path.basename(process.cwd())
             },
             {
                 type: 'input',
-                name: 'AEM_SERVER',
-                message: 'Set AEM URL (e.g. http://aem:4502)'
+                name: 'AEM_HOST',
+                message: 'AEM instance URL (e.g. https://author.adobeaemcloud.com)'
             },
             {
                 type: 'input',
-                name: 'AEM_GRAPHQL_ENDPOINT',
-                message: 'Set AEM GraphQL endpoint',
-                default: '/content/_cq_graphql/global/endpoint.json'
+                name: 'AEM_GRAPHQL',
+                message: 'AEM GraphQL endpoint',
+                default: '/content/_cq_graphql/global/endpoint.json',
+                dependsOn: ['AEM_HOST'],
             },
             {
                 type: 'input',
                 name: 'MAGENTO_GRAPHQL',
-                message: 'Set Magento GraphQL endpoint (e.g. http://magento:8080/graphql)',
+                message: 'Magento GraphQL endpoint (e.g. http://magento:8080/graphql)',
             }
         ]);
     }
 
     writing() {
         const props = {
-            NAME: this.answers.NAME,
-            AEM_SERVER: this.answers.AEM_SERVER,
-            AEM_GRAPHQL_ENDPOINT: this.answers.AEM_GRAPHQL_ENDPOINT,
+            NAME: this.answers.APP_NAME,
+            AEM_HOST: this.answers.AEM_HOST,
+            AEM_GRAPHQL: this.answers.AEM_GRAPHQL,
             MAGENTO_GRAPHQL: this.answers.MAGENTO_GRAPHQL
         };
 
--- generators/app/templates/next.config.js
+++ generators/app/templates/next.config.js
@@ -1,18 +1,19 @@
+require('dotenv').config();
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   // Compressing times out on Runtime ??...
   compress: false,
   // Use the CDN in production
-  assetPrefix: isProd ? 'https://53444-runtimenextjs.adobeio-static.net' : '',
+  assetPrefix: isProd ? `https://${process.env.AIO_runtime_namespace}.adobeio-static.net` : '',
   // For next/image
   images: {
-    domains: ['<%= AEM_SERVER %>']
+    domains: ['<%= AEM_HOST %>']
   },
   env: {
-    NAME: '<%= NAME %>',
-    AEM_SERVER: '<%= AEM_SERVER %>',
-    AEM_GRAPHQL_ENDPOINT: '<%= AEM_GRAPHQL_ENDPOINT %>',
+    NAME: '<%= APP_NAME %>',
+    AEM_SERVER: '<%= AEM_HOST %>',
+    AEM_GRAPHQL_ENDPOINT: '<%= AEM_GRAPHQL %>',
     MAGENTO_GRAPHQL: '<%= MAGENTO_GRAPHQL %>'
   },
   // Required for serverless
