0ed5338d086869d9741da071620f2485bb415b70
Stephan Ringel <ringel@adobe.com>
2022-03-18 17:08:11 +0100

    Fix issues in the template

--- generators/app/templates/next.config.js
+++ generators/app/templates/next.config.js
@@ -14,7 +14,7 @@ module.exports = {
     APP_NAME: '<%= APP_NAME %>',
     AEM_HOST: '<%= AEM_HOST %>',
     AEM_GRAPHQL: '<%= AEM_GRAPHQL %>',
-    MAGENTO_GRAPHQL: '<%= MAGENTO_GRAPHQL %>'
+    // MAGENTO_GRAPHQL: '<%= MAGENTO_GRAPHQL %>'
   },
   // Required for serverless
   experimental: {
